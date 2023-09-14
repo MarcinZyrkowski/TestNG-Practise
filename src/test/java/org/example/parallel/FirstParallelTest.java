62d7db9d7c015c89fa00f8f812ac2b28d5c31818
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2023-09-14 11:53:56 +0200

    added hierarchy of annotations test and parallel test execution test

---
+++ src/test/java/org/example/parallel/FirstParallelTest.java
@@ -0,0 +1,14 @@
+package org.example.parallel;
+
+import lombok.SneakyThrows;
+import org.testng.annotations.Test;
+
+public class FirstParallelTest {
+
+    @Test
+    @SneakyThrows
+    public void firstParallelTest() {
+        Thread.sleep(5000);
+    }
+
+}
